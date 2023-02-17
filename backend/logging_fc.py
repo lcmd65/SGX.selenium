b2b1b8865cbce0fbbd3f9648d926c99611e10a8e
Dat <dat.lemindast@gmail.com>
2023-02-17 19:25:20 +0700

    change

--- backend/logging_fc.py
+++ backend/logging_fc.py
@@ -1,5 +1,13 @@
 import logging
+from backend.string_define import type_of_data
 from backend.string_define import file_handler_url
+from backend.date_fc import *
+class log_data:
+    def __init__ (self, time_dl, file_dl):
+        self.time_dl = time_dl
+        self.file_dl = file_dl
+
+list_logging = []
 
 def logging_fc(file_name_input):
     log_format = '%(asctime)s -' +file_name_input+'- %(message)s'
@@ -10,3 +18,19 @@ def logging_fc(file_name_input):
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     logger.info("action from project")
+
+def logging_fc_traceback(file_name_log,curent_date_time, file_dload):
+    with open(file_name_log,'r') as f:
+        for line in f:
+            if curent_date_time in line:
+                if file_dload in line:
+                    return True
+        return False
+
+def check_all_logging_fc(curent_date_time,file_name_log):
+    for i in range(4):
+        for item in type_of_data:
+            if logging_fc_traceback(file_name_log, string_date(curent_date_time), item) == False:
+                list_logging.append(log_data(curent_date_time,))
+        curent_date_time = make_last_day(curent_date_time)
+    return list_logging
